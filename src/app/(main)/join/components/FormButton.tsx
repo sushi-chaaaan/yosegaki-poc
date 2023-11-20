3d929d38563eb05cc0b4237ca6d96e66d4d918c5
sushi-chaaaan <mail@sushichan.live>
2023-11-20 21:20:42 +0900

    refactor: show form link ig already signed in

---
+++ src/app/(main)/join/components/FormButton.tsx
@@ -0,0 +1,30 @@
+import { cookies } from "next/headers"
+import Link from "next/link"
+
+import SignInButton from "@/components/supabase/signInButton"
+import { buttonVariants } from "@/components/ui/styles/button"
+import { cn } from "@/lib/utils"
+import { createClient } from "@/utils/supabase/serverClient"
+
+export default async function FormButton() {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
+  const user = await supabase.auth.getUser()
+
+  return (
+    <div>
+      {user.data ? (
+        <Link
+          className={cn(buttonVariants({ className: "no-underline" }))}
+          href="/form"
+        >
+          <span>フォームへ(要Twitterアカウント)</span>
+        </Link>
+      ) : (
+        <SignInButton redirectTo="/form">
+          <span>フォームへ(要Twitterアカウント)</span>
+        </SignInButton>
+      )}
+    </div>
+  )
+}
