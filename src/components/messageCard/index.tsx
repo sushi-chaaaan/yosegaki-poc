224f6c5038077b915c094be3058feb2f57567f1c
sushi-chaaaan <mail@sushichan.live>
2023-11-22 07:32:33 +0900

    fix: MessageCard layout

--- src/components/messageCard/index.tsx
+++ src/components/messageCard/index.tsx
@@ -2,7 +2,6 @@ import Image from "next/image"
 
 import styles from "@/components/messageCard/message-card.module.scss"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { notoSansJP } from "@/lib/font"
 import { cn } from "@/lib/utils"
 import { FileWithUrl, User } from "@/messages/types"
 
@@ -15,30 +14,29 @@ type Props = {
 
 const MessageCard = ({ user, content, file, priorityLoading }: Props) => {
   return (
-    <div
-      className={cn("not-prose", styles.root, notoSansJP.className)}
-      key={user.id}
-    >
-      <div className="flex flex-row flex-nowrap items-center gap-x-4">
-        <Avatar>
-          <AvatarImage src={user.avatar_url} />
-          <AvatarFallback>{user.name}</AvatarFallback>
-        </Avatar>
-        <span className="text-xl font-bold">{user.name}</span>
-      </div>
-      <p>{content}</p>
-      <div className="relative aspect-golden w-full overflow-hidden">
-        {file && (
-          <Image
-            alt={file.name}
-            className="object-contain"
-            fill
-            priority={priorityLoading}
-            quality={100}
-            sizes="(max-width: 768px) 100vw, (max-width: 1280px) 50vw, 33vw"
-            src={file.url}
-          />
-        )}
+    <div className={styles.wrapper}>
+      <div className={cn("not-prose", styles.container)} key={user.id}>
+        <div className="flex flex-row flex-nowrap items-center gap-x-4">
+          <Avatar>
+            <AvatarImage alt={`avatar of ${user.name}`} src={user.avatar_url} />
+            <AvatarFallback>{user.name}</AvatarFallback>
+          </Avatar>
+          <span className="text-xl font-bold">{user.name}</span>
+        </div>
+        <p>{content}</p>
+        <div className="relative aspect-golden w-full overflow-hidden">
+          {file && (
+            <Image
+              alt={file.name}
+              className="object-contain"
+              fill
+              priority={priorityLoading}
+              quality={100}
+              sizes="(max-width: 768px) 100vw, (max-width: 1280px) 50vw, 33vw"
+              src={file.url}
+            />
+          )}
+        </div>
       </div>
     </div>
   )
