15bdf8516bb21413e66957a52a5f5492858416e5
sushi-chaaaan <mail@sushichan.live>
2023-11-22 07:09:18 +0900

    fix: middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,3 +24,16 @@ export async function middleware(request: NextRequest) {
     })
   }
 }
+
+// Ensure the middleware is only called for relevant paths.
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!_next/static|_next/image|favicon.ico).*)",
+  ],
+}
