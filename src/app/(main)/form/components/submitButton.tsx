34fd6ac9feceb20b1184c74006263b8ff7bc8426
sushi-chaaaan <mail@sushichan.live>
2023-12-03 22:49:45 +0900

    refactor: submit button props

--- src/app/(main)/form/components/submitButton.tsx
+++ src/app/(main)/form/components/submitButton.tsx
@@ -1,14 +1,27 @@
 "use client"
 
 import { ReloadIcon } from "@radix-ui/react-icons"
+import React from "react"
 import { useFormStatus } from "react-dom"
 
 import { Button } from "@/components/ui/button"
-const SubmitButton = ({ children }: { children?: React.ReactNode }) => {
+type Props = Omit<
+  React.ComponentPropsWithoutRef<"button">,
+  "type" | "children"
+> & {
+  children: React.ReactNode
+  type?: "submit"
+}
+const SubmitButton = ({
+  children,
+  type = "submit",
+  disabled,
+  ...rest
+}: Props) => {
   const { pending } = useFormStatus()
 
   return (
-    <Button disabled={pending} type="submit">
+    <Button disabled={pending || disabled} type={type} {...rest}>
       {pending && <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />}
       {children}
     </Button>
