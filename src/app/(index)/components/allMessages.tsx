aae27640306d2f1879c71ba9a02b40bf9a416054
sushi-chaaaan <mail@sushichan.live>
2023-12-04 00:59:33 +0900

    refactor: AllMessages

--- src/app/(index)/components/allMessages.tsx
+++ src/app/(index)/components/allMessages.tsx
@@ -1,24 +1,28 @@
-// import { unstable_cache } from "next/cache"
+import { unstable_cache } from "next/cache"
 
-// import { ACCEPTED_MESSAGES_CACHE_TAG } from "@/cache"
-// import Message from "@/components/messageCard"
+import { ACCEPTED_MESSAGES_CACHE_TAG } from "@/cache"
+import MessageCard from "@/components/messageCard"
+import { getAllValidYosegaki } from "@/messages/lib/yosegaki"
 
 export default async function AllMessages() {
-  // const getCachedAcceptedMessages = unstable_cache(
-  //   async () => await getAllAcceptedMessages(),
-  //   ["all-accepted-messages-display-to-top-page"],
-  //   {
-  //     tags: [ACCEPTED_MESSAGES_CACHE_TAG],
-  //     revalidate: 3600,
-  //   },
-  // )
-  // const messages = await getCachedAcceptedMessages()
+  const getCachedAcceptedMessages = unstable_cache(
+    async () => {
+      console.log("AllMessages: cache miss")
+      return await getAllValidYosegaki()
+    },
+    ["all-accepted-messages-display-to-top-page"],
+    {
+      tags: [ACCEPTED_MESSAGES_CACHE_TAG],
+      revalidate: 3600,
+    },
+  )
+  const messages = await getCachedAcceptedMessages()
 
   return (
-    <div className="grid grid-cols-1 p-4 md:grid-cols-2 xl:grid-cols-3">
-      {/* {messages.map((message) => (
-        <Message key={message.user.id} {...message} />
-      ))} */}
+    <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 xl:grid-cols-3">
+      {messages.map((message) => (
+        <MessageCard key={message.user.id} yosegaki={message} />
+      ))}
     </div>
   )
 }
