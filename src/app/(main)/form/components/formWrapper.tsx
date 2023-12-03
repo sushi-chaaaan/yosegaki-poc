3531c0185131ecb0ec1860bb35c73860c5b89a8e
sushi-chaaaan <mail@sushichan.live>
2023-12-03 19:09:00 +0900

    refactor: FormWrapper

--- src/app/(main)/form/components/formWrapper.tsx
+++ src/app/(main)/form/components/formWrapper.tsx
@@ -1,22 +1,18 @@
-import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
 import MessageForm from "@/app/(main)/form/components/messageForm"
-import { getMessage } from "@/supabase/lib/message"
-import { createClient } from "@/supabase/utils/serverClient"
+import { getYosegaki } from "@/messages/lib/yosegaki"
+import { getServerSession } from "@/supabase/lib/session"
 
 export default async function FormWrapper() {
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
-  const { data: sessionData, error: sessionError } =
-    await supabase.auth.getSession()
+  const { data: sessionData, error: sessionError } = await getServerSession()
   if (sessionError || sessionData.session == null) {
     console.error(sessionError)
     return redirect("/join")
   }
-  const message = await getMessage(cookieStore, sessionData.session)
-  // null => unauthorized
+
+  const message = await getYosegaki(sessionData.session.user.id)
   if (message == null) return redirect("/join")
 
-  return <MessageForm initialMessage={message} />
+  return <MessageForm initialYosegaki={message} />
 }
